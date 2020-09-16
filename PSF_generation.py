2900a7ad4cf38db79292d19649b8aa3aead96a4c
Danielemazzola95 <68697918+Danielemazzola95@users.noreply.github.com>
2020-09-16 10:03:25 +0200

    Update PSF_generation.py
    I coefficienti di Zernike sono distribuiti come frazioni di lambda con diversi andamenti: gaussiana, esponenziale e sigmoide selezionabili in input (con rispettive funzioni nel codice: gaussian_budget, exponential_budget, sigmoid_budget). Non sono i coefficienti in sé ad essere distribuiti, ma i denominatori che determinano quale frazione di lunghezza d’onda pesa ogni coefficiente. Se stampi ‘wf_error_budget’ vedi il “peso” che ha ogni coefficiente in differenza di cammino ottico (in metri).

--- PSF_generation.py
+++ PSF_generation.py
@@ -6,6 +6,7 @@ Created on Mon Aug 31 18:11:08 2020
 @author: Daniele Mazzola 
 """
 import numpy as np
+from scipy import signal
 import poppy
 import astropy.units as u
 import matplotlib.pyplot as plt
@@ -17,7 +18,7 @@ import time
 
 #%% ---------------------------------- DATA PATHS ------------------------------------------------------
 
-save_dir = os.path.join('..','..','TrainingSetZernike','test0','rr')
+save_dir = os.path.join('..','..','Training_Set_Zernike','test_0','rr')
 
 # If you want (change boolean):
 normalisation = False
@@ -28,34 +29,79 @@ crop = False
 # Random generation of Zernike coefficients
 def generate_coefficients(wavelenght, wfe_budget):
     coefficients = []
-    wavelength_m = wavelength.value * 1e-9
     for term in wfe_budget:
-        # terms must be in m
-        coefficients.append( wavelength_m / np.random.uniform(low = - term, high = + term) )
+        # Fractions of lambda (terms must be in m)
+        coefficients.append( np.random.uniform(low = - term, high = + term) )
     return coefficients
 
-# Cut the image in half (at the center)
+# Distributions of lambda fractions for Zernike coefficients:
+def sigmoid_budget(n_coeff):
+      # Lambda in m
+      wavelength_m = wavelength.value * 1e-9
+      # Sigmoid parameters
+      n_points = 4 * n_coeff
+      x = np.linspace(- n_coeff, + n_coeff, n_points)
+      ampl_0 = 1e3
+      shift_sigm = 2
+      # Sigmoid points for wf_error_budget (lambda fractions)
+      sigmoid = ampl_0 * (1 / (1 + np.exp( - 0.5 * x ))) + shift_sigm
+      sigmoid_sampled = wavelength_m / [round(value) for value in sigmoid[0 : n_points : 4]]
+      sigmoid_sampled[:3] = 0 # no piston and x,y tilts
+      return sigmoid_sampled
+
+def gaussian_budget(n_coeff):
+      # Lambda in m
+      wavelength_m = wavelength.value * 1e-9
+      # Gaussian parameters
+      n_points = 4 * n_coeff
+      standard_deviation = round(n_coeff / 2)
+      ampl_0 = 1e3
+      shift_gauss = 1
+      # Gaussian
+      gaussian = ampl_0 * signal.gaussian(M = n_points, std = standard_deviation) + shift_gauss
+      # Gaussian raising edge points for wf_error_budget (lambda fractions)
+      gauss_sampled = wavelength_m / [round(value) for value in gaussian[0 : int(n_points/2) : 2]]
+      gauss_sampled[: 3] = 0 # no piston and x,y tilts
+      return gauss_sampled
+
+def exponential_budget(n_coeff):
+      # Lambda in m
+      wavelength_m = wavelength.value * 1e-9
+      # Exp parameters
+      n_points = 4 * n_coeff
+      x = np.linspace(0, n_coeff, n_points)
+      ampl_0 = 0.2
+      # Shifted exponential
+      shift_exp = 0
+      exponential = ampl_0 * np.exp(x) + shift_exp
+      # Exponential points for wf_error_budget (lambda fractions)
+      exp_sampled = wavelength_m / [round(value) for value in exponential[0 : n_points : 4]]
+      exp_sampled[:3] = 0 # no piston and x,y tilts
+      return exp_sampled
+
+# Cut the image in half (in center)
 def crop_in_center(image, crop_size):
-  n_x = image.shape[0]
-  n_y = image.shape[1]
+  n_x = image.shape[1]
+  n_y = image.shape[0]
   l = crop_size // 2
   x_center, y_center = n_x // 2 + 1, n_y // 2 + 1
   cropped_image = image[x_center - l : x_center + l, y_center - l : y_center + l]
   return cropped_image
 
+# Create path if it does not exist yet
+def create_saving_directory(save_path):        
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+            
+# Create h5 file            
 def init_h5(save_dir):
     create_saving_directory(save_dir)
     timestamp = time.strftime("%y%m%d_%H%M%S", time.localtime())
     name = f'TrainingSet_{timestamp}.h5'
     filename = os.path.join(save_dir,name)
-    f = h5py.File(filename, 'w', libver='latest')
-    group = f.create_group('zernike_psf')  # Group 1
+    f = h5py.File(filename, 'w')
+    group = f.create_group('zernike_psf')  # 1 group
     return f, group
-
-def create_saving_directory(save_path):        
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
-
 
 #%% ----------------------------------- PARAMETERS --------------------------------------------------
 
@@ -64,46 +110,35 @@ SAMPLING = 0.25               # beam ratio (See: documentation)
 wavelength = 488 * u.nm
 NA = 0.3                      # Numerical Aperture
 fl_obj = 20 * u.mm            # objective focal length
-fl_tube = 200 * u.mm          # tube focal length
 delta = 0 * u.um              # defocus (at detector)
 pix_size = 0.1 * u.um         # detector pixel sizes
-n_pix = 256                   # pixels per side (detector)
+n_pix = 256                   # pixels per side (square detector)
 
-crop_size = int(n_pix / 2)
 
 # Other parameters
 radius = NA * fl_obj          # Aperture stop radius
+crop_size = int(n_pix / 2)
 
-# Zernike coefficients (in meters)
-n_coeff = 21
-n_set = 5                                 # number of images generated in the set
+# Zernike coefficients
+n_coeff = 20
+n_set = 10                    # number of images in the set
 
-#%% Zernike coefficients as lambda fraction
-wf_error_budget = [100]*n_coeff
-wf_error_budget[0] = 1e15
-wf_error_budget[1] = 1e15
-wf_error_budget[2] = 1e15
-# wf_error_budget[3] = 10
-# wf_error_budget[4] = 10
-# wf_error_budget[5] = 10
-# wf_error_budget[6] = 10
-# wf_error_budget[7] = 10
-# wf_error_budget[8] = 10
-# wf_error_budget[9] = 10
-# wf_error_budget[10] = 10
-# wf_error_budget[11] = 10
-# wf_error_budget[12] = 10
-# wf_error_budget[13] = 10
-# wf_error_budget[14] = 10
-# wf_error_budget[15] = 10
-# wf_error_budget[16] = 10
-# wf_error_budget[17] = 10
-# wf_error_budget[18] = 10
-# wf_error_budget[19] = 10
-# wf_error_budget[20] = 10
+#%% Zernike coefficients distribution as lambda fractions
 
+wf_error_budget = [] * n_coeff
 
-#%% ----------------------------------- OPTICAL ELEMENTS ----------------------------------------------
+print('Select desired coefficient distribution (default Sigmoid):')
+print('Sigmoid: 1 \nGaussian: 2 \nExponential: 3 \n')
+distribution = input()
+
+if distribution == 2:
+      wf_error_budget = gaussian_budget(n_coeff)
+elif distribution == 3:
+      wf_error_budget = exponential_budget(n_coeff)
+else:
+      wf_error_budget = sigmoid_budget(n_coeff)
+      
+#%% --------------------------------- OPTICAL ELEMENTS ----------------------------------------------
 
 aperture = poppy.CircularAperture(radius=radius, name='Pupil')
 
@@ -111,7 +146,7 @@ objective = poppy.QuadraticLens(fl_obj, name = 'Objective lens')
 
 # ----------------------------------- OPTICAL SYSTEM ------------------------------------------------
 
-# Initialize results as arrays
+# Initialize results as lists
 psf_results = []
 zernike_coeff = []
 
@@ -127,7 +162,7 @@ for image_idx in range(n_set):
     zernike_wfe = poppy.ZernikeWFE(radius = radius, coefficients = coefficients,
                              name = 'Aberrated Wavefront')
     
-    osys.add_optic(zernike_wfe)
+    osys.add_optic(zernike_wfe) # Non ideality
     
     osys.add_optic(objective, distance=fl_obj)
     
@@ -145,7 +180,7 @@ for image_idx in range(n_set):
     
     plt.figure(figsize=(10,10))
     poppy.display_psf(psf, normalize = 'total')
-    plt.title('objective lens PSF')
+    plt.title('objective lens PSF_{},'.format(image_idx))
     
     if crop:
           
@@ -159,20 +194,17 @@ for image_idx in range(n_set):
           psf_train = psf_train / np.max(psf_train)
           
     
-    # Update results 
-    
+    # Show results
     plt.figure(figsize=(10,10))
     plt.imshow(psf_train)
-    plt.title('objective lens PSF - cropped')
+    plt.title('objective lens PSF_cropped_{}'.format(image_idx))
     plt.show()      
                    
-    #%% Save results in h5 format
-    
+    # Save results in h5 format
     dataset_name = f't0/c0/aberrated_PSF_{image_idx:05d}'
     attribute_name = f'coefficients_{image_idx:05d}'
                 
-    data_h5 = group_h5.create_dataset(name = dataset_name,
-                                      data = psf_train)
+    data_h5 = group_h5.create_dataset(name = dataset_name, data = psf_train)
     data_h5.attrs[attribute_name] = coefficients
     data_h5.flush()
     
